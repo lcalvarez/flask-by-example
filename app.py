366e3dbbda84d07632cc25261a8beec2b1486b85
Luis Alvarez <luis.alvarez@stem.com>
2017-04-25 20:56:54 -0700

    added nltk download

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import operator
 import re
 import nltk
+nltk.download('punkt')
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
